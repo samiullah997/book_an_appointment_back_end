25d4449987b1f26e78ff6ef97ed471c620e77b54
Alusp <alusinejalloh800@gmail.com>
2023-01-18 13:16:45 +0200

    Update Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   extend Devise::Models
+  has_many :reservations, dependent: :destroy
+  has_many :bikes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
